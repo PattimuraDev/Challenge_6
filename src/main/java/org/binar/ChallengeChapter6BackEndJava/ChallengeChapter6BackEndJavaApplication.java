44e1270bf73912f4f1b04776f60be5cc3dbd8162
PattimuraDev <dwisatriapatra2000@gmail.com>
2022-11-30 15:13:27 +0700

    Rename docker file

--- src/main/java/org/binar/ChallengeChapter6BackEndJava/ChallengeChapter6BackEndJavaApplication.java
+++ src/main/java/org/binar/ChallengeChapter6BackEndJava/ChallengeChapter6BackEndJavaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChallengeChapter6BackEndJavaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ChallengeChapter6BackEndJavaApplication.class, args);
     }
-
 }
