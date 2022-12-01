f36af345352d6d213a55b3fb1815f746f9b8bc71
PattimuraDev <dwisatriapatra2000@gmail.com>
2022-12-01 20:21:38 +0700

    trying to solve cors problem

--- src/main/java/org/binar/ChallengeChapter6BackEndJava/configuration/CorsConfiguration.java
+++ src/main/java/org/binar/ChallengeChapter6BackEndJava/configuration/CorsConfiguration.java
@@ -1,10 +1,12 @@
 package org.binar.ChallengeChapter6BackEndJava.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class CorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
